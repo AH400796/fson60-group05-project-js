a97089c78bc2d9d91f9cd59408ced33764486906
Oksana-Zakhariia <okcahka.l@gmail.com>
2022-12-14 16:00:13 +0300

    addClose-Functions

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -5,15 +5,40 @@
     openModalBtn2: document.querySelector('[data-modal-open2]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('backdrop'),
+    modalCard: document.querySelector('.modal__film-card'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn1.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', onCloseModal);
+  refs.backdrop.addEventListener('click', onBackdropClick);
+
+
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal()
+  }
+};
+function onCloseModal() {
+  body.classList.remove('modal-open');
+
+};
+function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+        onCloseModal()
+    }    
+};
+function onCloseModal(event) {
+  body.classList.remove('modal-open');
+  modalCard.innerHTML = "";
+  window.removeEventListener('keydown', onEscKeyPress);
+}
