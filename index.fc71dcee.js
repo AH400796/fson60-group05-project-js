9412624d8da8af6fe193ba20679387ff3ad1830a
AH400796 <106314691+AH400796@users.noreply.github.com>
2022-12-14 14:08:32 +0000

    Deploying to gh-pages from @ AH400796/fson60-group05-project-js@368056bdc953fb3b9bf73bcf5ed21173249ae33a 🚀

--- index.fc71dcee.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={home:document.querySelector(".js-home"),library:document.querySelector(".js-library"),watched:document.querySelector(".js-watched"),queue:document.querySelector(".js-queue")},o=e.home;console.dir(o),o.onclick()}();
-//# sourceMappingURL=index.fc71dcee.js.map
