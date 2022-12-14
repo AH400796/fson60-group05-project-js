521579211911c8e26f3c37dcf101016b02b4ce60
AH400796 <106314691+AH400796@users.noreply.github.com>
2022-12-14 06:30:03 +0000

    Deploying to gh-pages from @ AH400796/fson60-group05-project-js@d957c662ab1a6050720075cb2794567775296451 🚀

---
+++ index.0ed2d142.js
@@ -0,0 +1,2 @@
+const{home:e,library:c,watched:o,queue:r}={home:document.querySelector(".js-home"),library:document.querySelector(".js-library"),watched:document.querySelector(".js-watched"),queue:document.querySelector(".js-queue")};console.dir(e),e.onclick();
+//# sourceMappingURL=index.0ed2d142.js.map
---
+++ index.fc71dcee.js
@@ -0,0 +1,2 @@
+!function(){var e={home:document.querySelector(".js-home"),library:document.querySelector(".js-library"),watched:document.querySelector(".js-watched"),queue:document.querySelector(".js-queue")},o=e.home;console.dir(o),o.onclick()}();
+//# sourceMappingURL=index.fc71dcee.js.map
