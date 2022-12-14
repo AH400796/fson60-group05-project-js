d957c662ab1a6050720075cb2794567775296451
AH400796 <A.Hokhman@gmail.com>
2022-12-14 08:29:35 +0200

    11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import './js/modal-open.js';
 // import './js/firebase';
 // import './js/fixed-header';
+import './js/header-navigation';
 
 // API_KEY = fbee7941f117d258bba2ad0706e433a4
 // BASE_URL = https://api.themoviedb.org/3/
---
+++ src/js/header-navigation.js
@@ -0,0 +1,9 @@
+const { home, library, watched, queue } = {
+  home: document.querySelector('.js-home'),
+  library: document.querySelector('.js-library'),
+  watched: document.querySelector('.js-watched'),
+  queue: document.querySelector('.js-queue'),
+};
+
+console.dir(home);
+home.onclick();
