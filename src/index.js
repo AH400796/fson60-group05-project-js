3e6d678742b5535c13d2766369ed33f439da647b
DarinaPilganchuk <106483724+DarinaPilganchuk@users.noreply.github.com>
2022-12-14 22:41:57 +0200

    створено футер додано стилі , додано зображення для майбутньої модалки

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import './js/modal-open.js';
 // import './js/firebase';
 // import './js/fixed-header';
+// import './js/footer-modal.js';
 import './js/header-navigation';
 
 // API_KEY = fbee7941f117d258bba2ad0706e433a4
