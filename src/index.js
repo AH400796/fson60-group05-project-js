56c2d731dce8226246bd7c0a4b90fbb0692189a4
AH400796 <A.Hokhman@gmail.com>
2022-12-15 23:12:22 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './js/modal-open.js';
+// import './js/modal-open.js';
 // import './js/firebase';
 // import './js/fixed-header';
 // import './js/footer-modal.js';
 import './js/header-navigation';
 import './js/search-header';
-import './js/get-trending';
+// import './js/get-trending';
 import './js/spinner';
 
 // API_KEY = fbee7941f117d258bba2ad0706e433a4
