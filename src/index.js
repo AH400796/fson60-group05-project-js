ac30f3b1a4f375ef0ef6c88bc6b979a0ebe165d4
AH400796 <A.Hokhman@gmail.com>
2022-12-15 10:36:04 +0200

    11

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 // import './js/fixed-header';
 // import './js/footer-modal.js';
 import './js/header-navigation';
+import './js/spinner';
 
 // API_KEY = fbee7941f117d258bba2ad0706e433a4
 // BASE_URL = https://api.themoviedb.org/3/
--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,13 +1,13 @@
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
-export function addSpinner() {
-    Loading.standard('Loading your movies...', {
-        svgColor: '$color-accent',
-        svgSize: '25px',
-        backgroundColor: '$color-overlay-modal',
-    });
-}
+export const addSpinner = function addSpinner() {
+  Loading.circle('Loading your movies...', {
+    svgColor: '$color-accent',
+    svgSize: '25px',
+    backgroundColor: '$color-overlay-modal',
+  });
+};
 
-export function removeSpinner() {
-    Loading.remove();
-}
+export const removeSpinner = function removeSpinner() {
+  Loading.remove();
+};
