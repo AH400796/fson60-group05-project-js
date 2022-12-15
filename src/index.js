a406a0d931090ee470c367077cbbb204bdbf3b22
AH400796 <A.Hokhman@gmail.com>
2022-12-16 00:13:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import './js/modal-open';
+// import './js/modal-open';
 // import './js/firebase';
 import './js/footer-modal.js';
 import './js/header-navigation';
 import './js/search-header';
-// import './js/get-trending';
+import './js/get-trending';
 import './js/add-to-watched-btn';
 import './js/spinner';
