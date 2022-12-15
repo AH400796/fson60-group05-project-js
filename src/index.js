d20a6a21ef63848ee94f3e0a72abb36cf2c58208
AH400796 <106314691+AH400796@users.noreply.github.com>
2022-12-15 12:11:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // import './js/modal-open.js';
 // import './js/firebase';
 // import './js/fixed-header';
+// import './js/footer-modal.js';
 import './js/header-navigation';
 import './js/search-header';
+import './js/get-trending';
+import './js/spinner';
 
 // API_KEY = fbee7941f117d258bba2ad0706e433a4
 // BASE_URL = https://api.themoviedb.org/3/
