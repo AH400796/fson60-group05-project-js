9a129226edae847c1d895c2be05fdfa55bce5d2a
Serhii Yatsenko <serhii2111@yahoo.com>
2022-12-16 02:04:12 +0200

    feature/createFilmCard

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 import './js/header-navigation';
 import './js/search-header';
 // import './js/get-trending';
+// import './js/renderGallery';
 import './js/spinner';
 
 // API_KEY = fbee7941f117d258bba2ad0706e433a4
--- src/js/search-header.js
+++ src/js/search-header.js
@@ -38,6 +38,15 @@ function createMarkup(data) {
   <p>${Number.parseInt(item.release_date)}</p>
   </div>
 </li>`;
+//     return `
+// <article class="filmCard" data-id="${id}">
+//   <img class="filmCard__img" srcset="${imgSrc(400)} 1x, ${imgSrc(500)} 2x" src="${imgSrc(400)}" width="280" height="398" alt="${title ? title : 'Poster'}" data-action="open-modal"/>
+//   <h2 class="filmCard-title">${title ? title : original_title ? original_title : 'No name'}</h2>
+//   <div class="filmCard-description">
+//     <p class="filmCard-genres">${genre_ids ? getFilmGenres(genre_ids) : 'Genre: -'}</p>
+//     <p class="filmCard-release">${release_date ? release_date.slice(0, 4) : 'Release date: -'}</p>${rating}
+//   </div>${libLabel}
+// </article>`;  
     })
     .join('');
   listFilms.innerHTML = markup;
