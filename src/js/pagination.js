4209031952b2514413e16da9e8feec70b9fc5daf
AH400796 <A.Hokhman@gmail.com>
2022-12-17 15:31:14 +0200

    1

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -5,21 +5,21 @@ let page = 1;
 
 const container = document.getElementById('pagination');
 const options = {
-	totalItems: 200,
-	itemsPerPage: 20,
-	visiblePages: 5,
-	usageStatistics: false,
-	page,
-	centerAlign: true,
-	firstItemClassName: 'tui-first-child',
-	lastItemClassName: 'tui-last-child',
-	template: {
-		page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-		currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-		moveButton: '<a href="#" class="tui-page-btn tui-{{type}}">' + '<span class="tui-ico-{{type}}">{{type}}</span>' + '</a>',
-		disabledMoveButton: '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' + '<span class="tui-ico-{{type}}">{{type}}</span>' + '</span>',
-		moreButton: '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' + '<span class="tui-ico-ellip">...</span>' + '</a>',
-	},
+  totalItems: 99,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  usageStatistics: false,
+  page,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton: '<a href="#" class="tui-page-btn tui-{{type}}">' + '<span class="tui-ico-{{type}}">{{type}}</span>' + '</a>',
+    disabledMoveButton: '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' + '<span class="tui-ico-{{type}}">{{type}}</span>' + '</span>',
+    moreButton: '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' + '<span class="tui-ico-ellip">...</span>' + '</a>',
+  },
 };
 
 const pagination = new Pagination(container, options);
