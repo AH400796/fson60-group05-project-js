a1987662ccf2494e2f5c6f1174c045990c2c179a
Serhii Yatsenko <serhii2111@yahoo.com>
2022-12-17 12:21:53 +0200

    pagination-step-one

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import './js/header-navigation';
 import './js/search';
 import './js/create-markup';
 import './js/get-trending';
+import './js/pagination';
+
 // import './js/add-to-watched-btn';
 import './js/spinner';
 import './js/scroll-up';
+
+
+
+// paganation.on('afterMove', (event) => {
+//      const currentPage = event.page;
+//      console.log(currentPage);
+// });
+
+
+    
+
