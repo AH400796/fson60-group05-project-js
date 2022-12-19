1bf1ba54e35c3a74236835a22fe334af0564ab81
Danik <95442609+BanduraDanik1@users.noreply.github.com>
2022-12-19 17:23:39 +0200

    add-modal-for-trailer

--- src/js/btn-trailer.js
+++ src/js/btn-trailer.js
@@ -1,16 +1,31 @@
-// import * as basicLightbox from 'basiclightbox'
+import * as basicLightbox from 'basiclightbox'
 
-// const instance = basicLightbox.create(`
-//     <iframe src="https://www.youtube.com/embed/E1oZhEIrer4" width="560" height="315" frameborder="0"></iframe>
-// `)
-// instance.show()
+const refs = {
+    instance: basicLightbox.create(`<div class="backdrop_trailer">
+        <div class="modal_trailer">
+            <iframe src="https://www.youtube.com/embed/E1oZhEIrer4" width="560" height="315" frameborder="0"></iframe>
+        </div>`),
+    body: document.body,
+    gallery: document.querySelector('.gallery__list'),
+    modalCard: document.querySelector('.modal__film-card'),
+}
 
-const gallery = document.querySelector('.gallery__list');
-gallery.addEventListener('click',makeBtnForTrailer)
-
-const modalCard = document.querySelector('.modal__film-card');
+refs.gallery.addEventListener('click',makeBtnForTrailer)
 
 function makeBtnForTrailer() {
     const markup = `<button class="btn_trailer"> Watch the trailer </button>`
-    modalCard.insertAdjacentHTML('beforeend', markup)
+    refs.modalCard.insertAdjacentHTML('beforeend', markup)
+    const btrForTrailer = document.querySelector('.btn_trailer')
+    btrForTrailer.addEventListener('click', openModalTrailer)
+}
+
+function openModalTrailer() {
+    refs.body.classList.add('show-modal_trailer');
+    refs.instance.show()
+    backdropEl = document.querySelector('.backdrop_trailer')
+    backdropEl.addEventListener('click',onCloseModal)
+}
+
+function onCloseModal() {
+    refs.instance.close()
 }
